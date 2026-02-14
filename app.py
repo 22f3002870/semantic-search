88f7c2f0b4fbe22bc28c1fb2354ede9d968cba61
pako-12 <22f3002870@ds.study.iitm.ac.in>
2026-02-15 01:44:51 +0530

    Enable CORS for grader

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from search import semantic_search
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/search", methods=["POST"])
 def search():
     data = request.json
